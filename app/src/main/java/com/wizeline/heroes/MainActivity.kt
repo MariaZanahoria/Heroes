bfa02c4671a4e3d7240629ad75b0415d10c61619
maria.carrillo <maria.carrillo@wizeline.com>
2022-02-04 22:09:54 -0600

    Add Splash and firebase config

--- app/src/main/java/com/wizeline/heroes/MainActivity.kt
+++ app/src/main/java/com/wizeline/heroes/MainActivity.kt
@@ -3,10 +3,12 @@ package com.wizeline.heroes
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
-import androidx.navigation.Navigation
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings
 import com.wizeline.heroes.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -14,9 +16,20 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.AppTheme)
+
+        val remoteConfig = FirebaseRemoteConfig.getInstance();
+        val isEnabled = remoteConfig.getBoolean("isAppBlocked")
+        val configSettings = FirebaseRemoteConfigSettings.Builder()
+            .setMinimumFetchIntervalInSeconds(3600)
+            .setFetchTimeoutInSeconds(60)
+            .build()
+        remoteConfig.setConfigSettingsAsync(configSettings)
+
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_fragment) as NavHostFragment
         val navController = navHostFragment.navController
